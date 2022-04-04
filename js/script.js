6fd42b4e1fb58f2d177f9645ca16f679fae5a320
Thomas Langan <tom.langan@comcast.net>
2022-04-04 14:39:11 -0400

    Include flex item properties Include grow, shrink and basis properties Allow the user to create an overflow condition

--- js/script.js
+++ js/script.js
@@ -6,29 +6,75 @@ addEventListener("load", onLoad);
 // helper functions for the executed code above
 
 function onLoad() {
+  //----------------------------------
+  // flex container properties
+  //----------------------------------
   // attach a 'change' event listener to the "flex wrap" select element.
-  document
-    .querySelector("select[name='flex-wrap']")
-    .addEventListener(
-      "change",
-      createChangePropertyEventListener("--flex-wrap")
-    );
+  if (document.querySelector("#flex-wrap")) {
+    document
+      .querySelector("#flex-wrap")
+      .addEventListener(
+        "change",
+        createChangePropertyEventListener("--flex-wrap")
+      );
+  }
 
   // attach a 'change' event listener to the "flex direction" select element.
-  document
-    .querySelector("select[name='flex-direction']")
-    .addEventListener(
-      "change",
-      createChangePropertyEventListener("--flex-direction")
-    );
+  if (document.querySelector("#flex-direction")) {
+    document
+      .querySelector("#flex-direction")
+      .addEventListener(
+        "change",
+        createChangePropertyEventListener("--flex-direction")
+      );
+  }
 
-  document
-    .querySelector(".add-flex-item")
-    .addEventListener("click", addFlexItem);
+  //----------------------------------
+  // flex item properties
+  //----------------------------------
+  // attach a 'change' event listener to the "flex-grow" textbox element.
+  if (document.querySelector("#flex-grow")) {
+    document
+      .querySelector("#flex-grow")
+      .addEventListener(
+        "change",
+        createChangePropertyEventListener("--flex-grow")
+      );
+  }
 
-  document
-    .querySelector(".remove-flex-items")
-    .addEventListener("click", removeFlexItems);
+  // attach a 'change' event listener to the "flex-shrink" textbox element.
+  if (document.querySelector("#flex-shrink")) {
+    document
+      .querySelector("#flex-shrink")
+      .addEventListener(
+        "change",
+        createChangePropertyEventListener("--flex-shrink")
+      );
+  }
+
+  // attach a 'change' event listener to the "flex-basis" textbox element.
+  if (document.querySelector("#flex-basis")) {
+    document
+      .querySelector("#flex-basis")
+      .addEventListener(
+        "change",
+        createChangePropertyEventListener("--flex-basis")
+      );
+  }
+
+  // attach a 'click' event listener to the "add-flex-item" button.
+  if (document.querySelector(".add-flex-item")) {
+    document
+      .querySelector(".add-flex-item")
+      .addEventListener("click", addFlexItem);
+  }
+
+  // attach a 'click' event listener to the "remove-flex-items" button.
+  if (document.querySelector(".remove-flex-items")) {
+    document
+      .querySelector(".remove-flex-items")
+      .addEventListener("click", removeFlexItems);
+  }
 
   // getProperty usage: cssGlobalProperties.getProperty("--some-property")
   // setProperty usage: cssGlobalProperties.setProperty("--some-property", "2")
@@ -70,8 +116,7 @@ function onLoad() {
 
   function CSSProperties(element) {
     function getProperty(propertyName) {
-      var rs = getComputedStyle(element);
-      return rs.getPropertyValue(propertyName);
+      return getComputedStyle(element).getPropertyValue(propertyName);
     }
     function setProperty(propertyName, propertyValue) {
       element.style.setProperty(propertyName, propertyValue);
